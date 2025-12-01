756cad23c389324788a862aa8769b5280c569dd6
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2025-12-01 10:23:11 +0000

    Updated animations (using delta)
    - Should be consistent speeds across browsers

--- animation.js
+++ animation.js
@@ -3,18 +3,18 @@
 
 // All of the rotation animations in the scene
 export function updateAnimation(delta, torus1, torus2, sphere, pole, pole2) {
-    torus1.rotation.x += 0.008;
-    torus1.rotation.y += 0.004;
-    torus1.rotation.z += 0.008;
+    torus1.rotation.x += 0.008 * delta * 60;
+    torus1.rotation.y += 0.004 * delta * 60;
+    torus1.rotation.z += 0.008 * delta * 60;
 
-    torus2.rotation.x += 0.016;
-    torus2.rotation.y += 0.008;
-    torus2.rotation.z += 0.016;
+    torus2.rotation.x += 0.016 * delta * 60;
+    torus2.rotation.y += 0.008 * delta * 60;
+    torus2.rotation.z += 0.016 * delta * 60;
 
-    sphere.rotation.y += 0.0025;
+    sphere.rotation.y += 0.0025 * delta * 60;
 
-    pole.rotation.y += 0.005;
-    pole2.rotation.y -= 0.005;
+    pole.rotation.y += 0.005 * delta * 60;
+    pole2.rotation.y -= 0.005 * delta * 60;
 
 }
 
